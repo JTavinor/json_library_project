90cef8a42b5717e17050ae5d4bd5e397ae5e7ea9
JTavinor <59698353+JTavinor@users.noreply.github.com>
2020-01-13 16:34:19 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+import functions
+
+MENU_PROMPT = """Do you want to:
+- Add a new genre to your library (g)
+- Add a new book to a genre (a)
+- View your library (v)
+- Search your library (s)
+- Mark a book in your library as read (r)
+- Delete something from your library (d)
+- Quit the app (q)
+Input: """
+
+MENU_OPTIONS = {
+    'g': functions.add_genre,
+    'a': functions.add_book,
+    'v': functions.view_library,
+    's': functions.search_menu,
+    'r': functions.mark_read,
+    'd': functions.prompt_delete
+}
+
+
+def menu():
+    while (user_input := input(MENU_PROMPT).lower().strip()) != "q":
+        try:
+            MENU_OPTIONS[user_input]()
+        except KeyError:
+            print("\nInvalid keyword, please try again\n")
+
+    print('\nGoodbye!')
+
+
+menu()
